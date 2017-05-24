caf48fe3afe90f06bdd238c29aa7a0e4be16729e
Harsimar Singh <harsimar.singh08@gmail.com>
2017-05-25 00:07:11 +0530

    Q-2

---
+++ C Questions/Q-2.c
@@ -0,0 +1,12 @@
+//Question 2:
+#include<stdio.h>
+void main()
+  {
+      float a;
+      double b;
+        printf("Enter the no");
+        scanf("%f",&a);
+        b=(double)a;
+        printf("The double=%lf",b);
+        printf("Float=%f",a);
+  }
