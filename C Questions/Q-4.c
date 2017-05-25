3b6183fb6d70cdb93edd28ab7e58daced210c482
Harsimar Singh <harsimar.singh08@gmail.com>
2017-05-26 02:19:50 +0530

    Q-4

---
+++ C Questions/Q-4.c
@@ -0,0 +1,17 @@
+//Question 2:
+// Write a program to find the size of all the data types.
+
+#include<stdio.h>
+void main()
+   {
+       printf("Size of short int=%d\n",sizeof(short int));
+       printf("Size of unsigned int=%d\n",sizeof(unsigned int));
+       printf("Size of signed int=%d\n",sizeof(signed int));
+       printf("Size of  int=%d\n",sizeof(int));
+       printf("Size of long int=%d\n",sizeof(long int));
+       printf("Size of float=%d\n",sizeof(float));
+       printf("Size of double=%d\n",sizeof(double));
+       printf("Size of char=%d\n",sizeof(char));
+       printf("Size of unsigned char=%d\n",sizeof(unsigned char));
+       printf("Size of signed char=%d\n",sizeof(signed char));
+   }
