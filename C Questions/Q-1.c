f20c6ba6984a69260f00ed53ec0e0807fb88de1e
Harsimar Singh <harsimar.singh08@gmail.com>
2017-05-25 23:05:54 +0530

    Q-3

--- C Questions/Q-1.c
+++
@@ -1,9 +0,0 @@
-//Question 1:
-#include<stdio.h>
-#include<limits.h>
-void main()
-    {
-      printf("The minimum value of INT = %d\n", INT_MIN);
-      printf("The maximum value of INT = %d\n", INT_MAX);
-    }
-
---
+++ C Questions/Q-3.c
@@ -0,0 +1,17 @@
+//Question 3: 
+//Write a program to convert double to float.
+
+#include<stdio.h>
+void main()
+  {	
+      float a;
+      double b;
+        printf("Enter the no");
+        scanf("%lf",&b);
+        a=(float)b;
+        printf("The double=%lf",b);
+        printf("THE Float=%f",a);
+  }
+
+
+
