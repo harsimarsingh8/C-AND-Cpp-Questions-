326f5f8766ccde357536ce0ca255fdb6e29c864e
Harsimar Singh <harsimar.singh08@gmail.com>
2017-05-29 10:39:25 +0530

    Q-1

---
+++ C Questions/Q-1.c
@@ -0,0 +1,10 @@
+//Question 1: 
+//Write a program to print all the possible values an integer variable can have..
+
+#include<stdio.h>
+#include<limits.h>
+void main()
+    {
+      printf("The minimum value of INT = %d\n", INT_MIN);
+      printf("The maximum value of INT = %d\n", INT_MAX);
+    }
