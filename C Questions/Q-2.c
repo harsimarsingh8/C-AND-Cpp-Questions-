008508ed993fc8491673e3fffde1d328119754de
Harsimar Singh <harsimar.singh08@gmail.com>
2017-05-29 10:44:50 +0530

    Q-2

---
+++ C Questions/Q-2.c
@@ -0,0 +1,14 @@
+//Question 2: 
+//Write a program to convert float to double..
+
+#include<stdio.h>
+void main()
+  {
+      float a;
+      double b;
+        printf("Enter the no");
+        scanf("%f",&a);
+        b=(double)a;
+        printf("The double=%lf",b);
+        printf("Float=%f",a);
+  }
