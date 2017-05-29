b92cfb67468a11010592ab8290b2a3978ae37907
Harsimar Singh <harsimar.singh08@gmail.com>
2017-05-29 11:26:56 +0530

    Q-5

---
+++ C Questions/Q-5.c
@@ -0,0 +1,13 @@
+//Question 5: 
+//Write a program to convert decimal number to octal number.
+
+
+#include<stdio.h>
+void main()
+     {
+      int a;
+       printf("Enter the decimal no:");
+       scanf("%d",&a);
+       printf("Octal no=%o",a);
+     }
+
