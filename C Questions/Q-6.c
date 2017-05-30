d8925cc6932691563bbf4874e7fd089cb408e88a
Harsimar Singh <harsimar.singh08@gmail.com>
2017-05-30 10:40:17 +0530

    Q-6

---
+++ C Questions/Q-6.c
@@ -0,0 +1,13 @@
+//Question 6: 
+//Write a program to convert decimal number to hexadecimal number.
+
+
+ #include<stdio.h>
+void main()
+     {
+      int a;
+       printf("Enter the decimal no:");
+       scanf("%d",&a);
+       printf("Hexadecimal no=%x",a);
+     }
+
