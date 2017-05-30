5bf3c1214d0d854b2315ea94256749f690f351a4
Harsimar Singh <harsimar.singh08@gmail.com>
2017-05-30 10:43:23 +0530

    Q-7

---
+++ C Questions/Q-7.c
@@ -0,0 +1,15 @@
+//Question 7: 
+// Write a program to print formatted output.
+
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    printf("Hello\bworld!\n");//Backspace
+    printf("Hello\tworld!\n");//tab space
+    printf("Hello\nworld!\n");//New line
+    printf("Hello\rworld!");//Carriage return
+    return 0;
+}
+
