3a487a03533eacb27c9c4802df9de5d368ac4f28
Harsimar Singh <harsimar.singh08@gmail.com>
2017-06-01 10:20:12 +0530

    Q-11

---
+++ C Questions/Q-11.c
@@ -0,0 +1,22 @@
+//Question 11: 
+// C Program to Check if a given Integer is Power of 2 using Bitwise Operators. .
+
+
+
+#include<stdio.h>
+void main()
+{
+  int n,count=0;
+  printf("Enter the no");
+  scanf("%d",&n);
+  while(n!=0)
+   {
+    if(n&1)
+    count++;
+    n=n>>1;
+   }
+   if(count==1)
+      printf("The no is power of two");
+  else
+    printf("Not power of two");
+}
