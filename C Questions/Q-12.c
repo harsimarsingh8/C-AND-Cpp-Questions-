a5b77381bcb51e58b5f274a12a552fca54bb91a9
Harsimar Singh <harsimar.singh08@gmail.com>
2017-06-02 21:38:37 +0530

    Q-12

---
+++ C Questions/Q-12.c
@@ -0,0 +1,15 @@
+//Question 12: 
+// WAP to swap two numbers using bitwise XOR Operator.
+
+
+#include<stdio.h>
+void main()
+{
+	 int i,j,k;
+	 printf("Enter the number i and j: ");
+	 scanf("%d %d",&i,&j);
+	 i=i^j;
+	 j=i^j;
+	 i=i^j;
+	 printf("i= %d ; j = %d\n",i,j);
+}
