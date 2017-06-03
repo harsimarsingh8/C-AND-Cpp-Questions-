d47be0d6c919c18c6fb31ab699efbe616dd81b33
Harsimar Singh <harsimar.singh08@gmail.com>
2017-06-03 06:07:50 +0530

    Q-13

---
+++ C Questions/Q-13.c
@@ -0,0 +1,22 @@
+//Question 13: 
+// Write a C program to find the factorial of a number, where the number is entered by user.
+
+
+#include<stdio.h>
+int fb(int n)
+    {
+      int c, fact = 1;
+      for (c = 1; c <= n; c++)
+      fact = fact * c;
+      return fact;
+       }
+    void main()
+         {
+          int a,b;
+          printf("Enter the no");
+          scanf("%d",&a);
+          b=fb(a);
+         printf("Factorial=%d",b);
+
+         }
+
