55b543273b1aa28c7c6a2de9122ab3d6a5f1c34d
Harsimar Singh <harsimar.singh08@gmail.com>
2017-09-18 19:39:04 +0530

    Q-1

--- C Questions/Q-1.c
+++ C Questions/Q-1.c
@@ -5,6 +5,6 @@
 #include<limits.h>
 void main()
     {
-      printf("The minimum value of INT = %d\n", INT_MIN);
+      printf("The minimum value of INT = %d\n", INT_MIN);    
       printf("The maximum value of INT = %d\n", INT_MAX);
     }
